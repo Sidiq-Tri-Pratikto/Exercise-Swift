95a2f6ac0fca9b1810e6b5b79c76766f7a6e00c6
Sidiq Tri Pratikto <sidiqtripratikto@GDPL-LTP-242.local>
2024-09-20 22:58:05 +0700

    add learning codes swift

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Sidiq Tri Pratikto on 19/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
